ee423ff5df6db40932fc87ed88cdcabb36bd626b
Artem <you@example.com>
2021-10-14 20:12:40 +0300

    Threading daemon

---
+++ lesson_3_deamon_thread.py
@@ -0,0 +1,48 @@
+import threading
+import time
+
+total = 4
+
+def create_item():
+    global total
+    # В каждой итерации будем делать изменения глобальной переменной total
+    for i in range(3):
+        time.sleep(2)
+        print(f'iteration first method {i}')
+        total +=1
+    print('iterations finished')
+
+def create_items_2():
+    global total
+    # В каждой итерации будем делать изменения глобальной переменной total
+    for i in range(3):
+        time.sleep(1)
+        print(f'\niteration second method {i}')
+        total += 1
+    print('iterations finished')
+
+# Будет контролировать лимт глобальной переменной total
+def limits_items():
+    global total
+    while True:
+        if total > 5:
+            print('overloaded')
+            total -=3
+            print('subtracted by 3')
+        else:
+            time.sleep(1)
+            print('Waiting')
+
+create1 = threading.Thread(target=create_item)
+create2 = threading.Thread(target=create_items_2)
+limiter = threading.Thread(target=limits_items, daemon=True) # Daemaon - значит что поток остановиться, когда все потоки остальные перестанут работать
+
+create1.start()
+create2.start()
+limiter.start()
+
+create1.join()
+create2.join()
+# limiter.join()
+
+print("Finish all")
---
+++ threading_lesson_2_list_thread.py
@@ -0,0 +1,36 @@
+import threading
+import time
+
+"""
+Здесь создаем неупорядоченные список потоков
+
+"""
+
+def sleeper(n, name):
+    print(f'Hi I am {name}, I am going to sleep')
+    time.sleep(n)
+    print(f'{name} has waked up')
+
+
+t = threading.Thread(target=sleeper, name='Thread optional', args=(5, 'Vasya')) # name - имя потока, # Создаем поток и в target засовываем нужную фунцкцию, args = аргументы для фунции в target
+
+thread_list = []
+
+start = time.time()
+
+for i in range(5):
+    t = threading.Thread(target=sleeper,
+                         name=f'Thread{i}',
+                         args=(5, f'Thread{i}'))
+    thread_list.append(t)
+    t.start()
+    print(f"{t.name} has started")
+
+for t in thread_list:
+    t.join()
+
+end = time.time()
+
+print(f"Full time {end-start}")
+
+print("All threads done")
